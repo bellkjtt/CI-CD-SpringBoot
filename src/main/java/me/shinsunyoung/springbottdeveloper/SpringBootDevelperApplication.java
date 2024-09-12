0ba703e55ae87e911753e142a6559ec1d897c22b
bellkjtt <bellkjtt@gmail.com>
2024-09-13 08:57:31 +0900

    first commit

---
+++ src/main/java/me/shinsunyoung/springbottdeveloper/SpringBootDevelperApplication.java
@@ -0,0 +1,11 @@
+package me.shinsunyoung.springbottdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevelperApplication {
+    public  static void main(String[] args){
+        SpringApplication.run(SpringBootDevelperApplication.class, args);
+    }
+}
